6d0bf998246c028f029b3db722dd3f95f15fac12
wraetz <raetzw@ncbi.nlm.nih.gov>
2018-03-15 15:07:48 -0400

    improved java classes

--- backend/1.sh
+++ backend/1.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-execute()
-{
-    echo $1
-    eval $1
-}
-
 #
 # in other terminal on same machine call 'nc -lk 9999'
 #
 
-#SPARK_HOME="$HOME/spark/JARS.2.2"
 SPARK_HOME="/usr/local/spark/2.2.0/jars"
 JAVA_HOME="$(dirname $(dirname $(readlink -f $(which javac))))"
-JAVA_INC=" -I$JAVA_HOME/include -I$JAVA_HOME/include/linux"
 
 BLASTJNI="gov.nih.nlm.ncbi.blastjni.BlastJNI"
 DEPENDS="$SPARK_HOME/*:$BLASTJNI:."
@@ -21,24 +13,51 @@ DEPENDS="$SPARK_HOME/*:$BLASTJNI:."
 MAIN_JAR="./gcp_blast.jar"
 MAIN_CLASS="GCP_BLAST"
 
-compile()
+compile_blast_jni()
 {
-    echo "compiling:"
-    
-    #compile the JNI-interface
-	rm -rf *.so  
-    echo "g++ -Wall -g -fPIC -shared $JAVA_INC -o libblastjni.so .src/blastjni.cpp"
-    g++ -Wall -g -fPIC -shared $JAVA_INC -o ./libblastjni.so ./src/blastjni.cpp
+    rm -rf *.so
+    echo "compiling and linking blastjni.so"
+    g++ src/blastjni.cpp \
+        -L ./int/blast/libs \
+        -std=gnu++11 \
+        -Wall \
+        -g \
+        -I . \
+        -shared \
+        -fPIC \
+        -I $JAVA_HOME/include -I$JAVA_HOME/include/linux \
+        -I /netopt/ncbi_tools64/c++.stable/include \
+        -I /netopt/ncbi_tools64/c++.stable/GCC493-ReleaseMT/inc \
+        -L /netopt/ncbi_tools64/c++.stable/GCC493-ReleaseMT/lib \
+        -L . \
+        -L ext \
+        -fopenmp -lxblastformat -lalign_format -ltaxon1 -lblastdb_format \
+        -lgene_info -lxformat -lxcleanup -lgbseq -lmlacli -lmla -lmedlars \
+        -lpubmed -lvalid -ltaxon3 -lxalnmgr -lblastxml -lblastxml2 -lxcgi \
+        -lxhtml -lproteinkmer -lxblast -lxalgoblastdbindex -lcomposition_adjustment \
+        -lxalgodustmask -lxalgowinmask -lseqmasks_io -lseqdb -lblast_services \
+        -lxalnmgr -lxobjutil -lxobjread -lvariation -lcreaders -lsubmit \
+        -lxnetblastcli -lxnetblast -lblastdb -lscoremat -ltables -lxregexp \
+        -lncbi_xloader_genbank -lncbi_xreader_id1 -lncbi_xreader_id2 \
+        -lncbi_xreader_cache -lncbi_xreader_pubseqos -ldbapi_driver \
+        -lncbi_xreader -lxconnext -lxconnect -lid1 -lid2 -lxobjmgr \
+        -lgenome_collection -lseqedit -lseqsplit -lsubmit -lseqset -lseq \
+        -lseqcode -lsequtil -lpub -lmedline -lbiblio -lgeneral -lxser -lxutil \
+        -lxncbi -lxcompress -llmdb -lpthread -lz -lbz2 \
+        -L/netopt/ncbi_tools64/lzo-2.05/lib64 -llzo2 -ldl -lz -lnsl -ldw -lrt -ldl -lm -lpthread \
+        -o blastjni.so
+}
 
-	#compile the java-sources into classes
-	rm -rf *.class
-    echo "javac -Xlint:unchecked -cp $DEPENDS -d . src/*.java"
+compile_blast_java()
+{
+    rm -rf *.class
+    echo "compiling java-classes"
 	javac -Xlint:unchecked -cp $DEPENDS -d . src/*.java
 }
 
 package()
 {
-	#package all compiled classes into a jar ( for execution we still need classpaths to SPARK )
+	#package all compiled classes into a jar
 	#make the jar we package up contain all it's dependencies...
 	jar cf $MAIN_JAR *.class
 	rm -rf *.class
@@ -46,19 +65,45 @@ package()
 
 run_local()
 {
-    echo "run:"
+    echo "running:"
 	echo "java -cp $MAIN_JAR:$DEPENDS $MAIN_CLASS"
+    export LD_LIBRARY_PATH=".:./ext:/opt/ncbi/gcc/4.9.3/lib64/"
+    export BLASTDB="/net/napme02/vol/blast/db/blast"
 	java -cp $MAIN_JAR:$DEPENDS $MAIN_CLASS
 }
 
+run_test()
+{
+    echo "running test:"
+	echo "java -cp $MAIN_JAR:$DEPENDS BlastJNI"
+    export LD_LIBRARY_PATH=".:./ext:/opt/ncbi/gcc/4.9.3/lib64/"
+    export BLASTDB="/net/napme02/vol/blast/db/blast"
+	java -cp $MAIN_JAR:$DEPENDS BlastJNI
+}
 
-compile
-
+compile_blast_jni
 retval=$?
-if [[ $retval -eq 0 ]]; then
-    package
-    retval=$?
-    if [[ $retval -eq 0 ]]; then
-        run_local
-    fi
+if [[ $retval -ne 0 ]]; then
+    echo "compile_blast_jni failed"
+    exit
 fi
+echo "compile_blast_jni success"
+
+compile_blast_java
+retval=$?
+if [[ $retval -ne 0 ]]; then
+    echo "compile_blast_java failed"
+    exit
+fi
+echo "compile_blast_java success"
+
+package
+retval=$?
+if [[ $retval -ne 0 ]]; then
+    echo "package jar failed"
+    exit
+fi
+echo "package jar success"
+
+#run_test
+run_local
