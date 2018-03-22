e6fbf45728d6f41c2c5e55bc75af57fe555f3aef
wraetz <raetzw@ncbi.nlm.nih.gov>
2018-03-22 14:10:17 -0400

    merging

--- sprint2_wolfgang/src/BlastJNI.java
+++ sprint2_wolfgang/src/BlastJNI.java
@@ -21,58 +21,178 @@
  */
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.lang.String;
 import java.lang.System;
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.channels.FileLock;
+import java.nio.channels.FileChannel;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.BucketInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.Storage.BlobListOption;
+import com.google.cloud.storage.StorageOptions;
 
 import org.apache.spark.*;
 import org.apache.spark.SparkFiles;
 
-public class BlastJNI
-{
-    static
-    {
-        try
-        {
-            System.load( SparkFiles.get( "libblastjni.so" ) );
-        }
-        catch ( Exception e )
-        {
-            System.out.println( "System.load() exception: " + e );
+public class BlastJNI {
+    static {
+        try {
+            // Java will look for libblastjni.so
+            System.loadLibrary("blastjni");
+            //System.load(SparkFiles.get("libblastjni.so"));
+        } catch(Exception e) {
+            try {
+                log("Couldn't System.loadLibrary, trying System.load");
+                System.load(SparkFiles.get("libblastjni.so"));
+            } catch(Exception e2) {
+                log("System.load() exception: " + e2);
+            }
         }
     }
 
-    public static void log( String msg )
-    {
-        try
-        {
-            PrintWriter pw = new PrintWriter( new FileOutputStream( new File( "/tmp/blastjni.java.log" ), true ) );
-            pw.println( msg );
+    private native String[] prelim_search(String dbenv, String rid, String query, String db_part, String params);
+
+    private native String[] traceback(String[] jsonHSPs);
+
+    public static void log(String msg) {
+        try {
+            System.out.println(msg);
+            PrintWriter pw=new PrintWriter(new FileOutputStream(new File("/tmp/blastjni.log"), true));
+            pw.println("(java) "+ msg);
             pw.close();
-        } catch ( FileNotFoundException ex )
-        {}
+        } catch(FileNotFoundException ex) {
+        }
     }
 
-    private native String[] prelim_search( String jobid, String query, String db_part, String params );
+    private String cache_dbs(String db_bucket, String db, String part) {
+        log("cache_dbs(db_bucket=" + db_bucket + ", db=" + db + ", part=" + part + ")");
 
-    public String[] jni_prelim_search( String jobid, String query, String db_part, String params )
+        String localdir="/tmp/blast/";
+        String dbdir=localdir + part + "/";
+        String donefile=dbdir + "done";
+        String lockfile=dbdir + "lock";
+
+        // This requires 118MB of .jars, could also just invoke cache_files.sh
+        // which would invoke gsutil
+        if(!Files.exists(Paths.get(donefile))) {
+            log(donefile + " doesn't exist. Checking if locked...");
+            try {
+                File dir=new File(dbdir);
+                dir.mkdirs();
+
+                File flock=new File(lockfile);
+                FileChannel channel=new RandomAccessFile(flock, "rw").getChannel();
+                FileLock lock=channel.lock();
+                // ^^^ blocks
+                if(!Files.exists(Paths.get(donefile))) {
+                    log(donefile + " still doesn't exist. This thread will lock and download.");
+                    Storage storage=StorageOptions.getDefaultInstance().getService();
+
+                    log("Got storage for bucket " + db_bucket);
+
+                    Bucket bucket=storage.get(db_bucket);
+                    for(Blob blob : bucket.list(
+                                Storage.BlobListOption.prefix(part + ".")).
+                            iterateAll()) {
+                        String dbfile=blob.getName();
+                        String ext=dbfile.substring(dbfile.length() - 4);
+                        if(ext.endsWith("sq") || ext.endsWith("in")) {
+                            Path path=Paths.get(dbdir + db + ext);
+                            log("    Downloading " + dbfile +
+                                    " -> " + path + " ...");
+                            blob.downloadTo(path);
+                        } else {
+                            log("    Skipping " + dbfile);
+                        }
+                            }
+
+                    // Create donefile
+                    File fdone=new File(donefile);
+                    fdone.createNewFile();
+                    log("Created " + donefile);
+
+                    flock.delete();
+                }
+
+                if(lock != null) {
+                    lock.release();
+                }
+                channel.close();
+            } catch(Exception e) {
+                log("exception in cache method: " + e);
+            }
+        } else
+        {
+            log(dbdir + " cached.");
+        }
+
+        return dbdir;
+    }
+
+    //private native String[] traceback(String[] jsonHSPs);
+    public String[] jni_traceback(String[] jsonHSPs)
     {
-        log( "jni_prelim_search called with "+query+","+db_part+","+params );
-        String[] results = prelim_search( jobid, query, db_part, params );
-        log( "jni_prelim_search returned " + results.length + " results" );
+        log("jni_traceback called with ");
+        for (String s: jsonHSPs)
+        {
+            log("hsp:" + s);
+        }
+
+        //String dbenv=cache_dbs(db_bucket, db, part);
+
+        String[] results=traceback(jsonHSPs);
+        log("jni_traceback returned " + results.length + " results:");
+        for (String s: results)
+        {
+            log("tb:" + s);
+        }
+
         return results;
     }
 
-    public static void main( String[] args )
-    {
-        String r_id   = "ReqID123";
-        String query  = "CCGCAAGCCAGAGCAACAGCTCTAACAAGCAGAAATTCTGACCAAACTGATCCGGTAAAACCGATCAACG";
-        String db     = "nt.04";
-        String params = "blastn";
-        
-        String results[] = new BlastJNI().jni_prelim_search( r_id, query, db, params );
-        
-        System.out.println( "Java results[] has " + results.length + " entries:" );
-        System.out.println( Arrays.toString( results ) );
+    public String[] jni_prelim_search(String db_bucket, String db, String rid, String query, String part, String params) {
+        log("jni_prelim_search called with " + db_bucket + "," + db + "," + rid + "," + query + "," + part + "," + params);
+        String dbenv=cache_dbs(db_bucket, db, part);
+
+        String[] results=prelim_search(dbenv, rid, query, db, params);
+        log("jni_prelim_search returned " + results.length + " results");
+        return results;
+    }
+
+
+    public static void main(String[] args) {
+        String rid  ="ReqID123";
+        String query ="CCGCAAGCCAGAGCAACAGCTCTAACAAGCAGAAATTCTGACCAAACTGATCCGGTAAAACCGATCAACG";
+        String db    ="nt";
+        String db_bucket=db + "_50mb_chunks";
+        String params="blastn";
+
+        ArrayList<String> al=new ArrayList<String>();
+        for (int partnum=0; partnum <= 26; ++partnum)
+        {
+            String part=db + "_50M." + String.format("%02d", partnum);
+
+            String results[]=new BlastJNI().jni_prelim_search(db_bucket, db, rid, query, part, params);
+
+            log("Java results[] has " + results.length + " entries:");
+            log(Arrays.toString(results));
+            al.addAll(Arrays.asList(results));
+        }
+
+        String hsp[]=al.toArray(new String[0]);
+        String[] tracebacks=new BlastJNI().jni_traceback(hsp);
     }
 }
+
