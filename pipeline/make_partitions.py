8aba4d5619c23a068dae48518724952ea3a8b58c
Mike Vartanian <Mike.Vartanian@nih.gov>
2018-03-22 11:02:59 -0400

    column rename

--- pipeline/make_partitions.py
+++ pipeline/make_partitions.py
@@ -4,5 +4,5 @@ dbs=["nt", "nd.02", "alligator"]
 for db in dbs:
 #    print "{ \"DB\": \"%s\", \"partition\": \"%s\"}" % (db,db)
     for x in range(886):
-        print "{ \"DB\": \"%s\", \"partition\": \"%s_50M.%0.2d\"}" % (db,db,x)
+        print "{ \"DB\": \"%s\", \"part\": \"%s_50M.%0.2d\"}" % (db,db,x)
 
