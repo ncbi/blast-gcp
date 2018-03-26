99f3121286060b0e529b1c73448e423689f9983d
wraetz <raetzw@ncbi.nlm.nih.gov>
2018-03-26 10:37:50 -0400

    worker initializer

---
+++ sprint2_wolfgang/init_worker.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+sudo apt-get install asn1c python-pyasn1 dumpasn1 libtasn1-bin libdw-dev -y
+
+BLASTTMP=/tmp/blast/
+BLASTDBDIR=$BLASTTMP/db
+
+MAXJOBS=8
+parts=`gsutil ls gs://nt_50mb_chunks/  | cut -d'.' -f2 | sort -nu`
+for part in $parts; do
+    piece="nt_50M.$part"
+    mkdir -p $BLASTDBDIR/$piece
+    cd $BLASTDBDIR/$piece
+    #mkdir lock
+    gsutil -m cp gs://nt_50mb_chunks/$piece.*in . &
+    gsutil -m cp gs://nt_50mb_chunks/$piece.*sq . &
+    touch done
+    #rmdir lock
+
+    j=`jobs | wc -l`
+    while [ $j -ge $MAXJOBS ]; do
+        j=`jobs | wc -l`
+        echo "$j waiting ..."
+        sleep 0.5
+    done
+done
+
+chown -R spark:spark /tmp/blast/
+chmod -R ugo+rw /tmp/blast
+
+echo Cluster Initialized
+date
+exit 0
