401030e5280c4a38ff59baef54a9a3de6d8f702d
Mike Vartanian <Mike.Vartanian@nih.gov>
2018-04-12 10:57:30 -0400

    Preliminary pricing evaluation

--- lib_builder/make_cluster.sh
+++ lib_builder/make_cluster.sh
@@ -9,7 +9,9 @@ PIPELINEBUCKET="gs://blastgcp-pipeline-test"
 # Larger nodes have faster network, and faster startup
 # Takes an n1-standard-32 about 11 minutes to copy 50GB NT
 # in parallel. No improvement seen by tar'ing them all up into one object to
-# downloaed.
+# download.
+#
+# FIX: nr is 196GB+
 #
 # IFF blast is memory bandwidth bound, n1-standard-8 appears to have similar
 # single core memory bandwidth as n1-standard-32. Will need to benchmark
@@ -19,6 +21,7 @@ PIPELINEBUCKET="gs://blastgcp-pipeline-test"
 # ~13% higher in NoVA (us-east4) versus
 #                South Carolina (us-east1) /
 #                Iowa (us-central1)
+# 96 core not available in us-east4, us-west1-c, us-east1-b
 # n1-standard-N: N=1,2,...64,96
 #   $0.0535/$0.0107 per hour (normal/preemptible)
 #   N vCPU
@@ -29,18 +32,36 @@ PIPELINEBUCKET="gs://blastgcp-pipeline-test"
 # n1-highmem-N: N=2,4,64,96
 #   $0.0674/$0.0134 per hour ("") (so ~25% more expensive)
 #
-# plus DataProc surcharge ($0.01/vCPU)
+# plus DataProc surcharge ($0.01/vCPU): ** Half of our expense?!? **
 # plus provisioned storage ($0.044/ GB month)
-# minus sustained use (25-30%) and committed use (~50-60%)
+# minus sustained use (25-30%), committed use (~40-60%), and preemptible (80%)
+#
+# Memory must be 0.9-6.5GB/vCPU
+#
+# If we hypothetically require 1000 vCPUs to meet SLA,
+# cheaper to have 11 96-core machines, with 11 disks?
+#
+# But at least two workers must be non-preemptible (5X $), so need
+# to balance disk costs versus workers.
+#
+# GCP' pricing calculator:
+#  32 standard-32: $17,347/month
+#  16 highcpu-64:  $14,932/month
+#  32 highcpu-32:  $15,113/month
+#  64 highcpu-16:  $14,079/month
+#  64 highcpu-16:  $13,607/month (limited disk)
+# 256 highcpu-4:   $15,581/month
+#  16 highmem-64:  $19,535/month (limited disk, all ramdisk)
+# Ex: $11,666/month (12 std-64)
 gcloud dataproc --region us-east4 \
     clusters create cluster-$USER-$(date +%Y%m%d) \
     --master-machine-type n1-standard-8 \
         --master-boot-disk-size 100 \
     --num-workers 2 \
-    --worker-machine-type n1-standard-32 \
-        --worker-boot-disk-size 100 \
-    --num-preemptible-workers 2 \
-        --preemptible-worker-boot-disk-size 100 \
+    --worker-machine-type n1-highcpu-16 \
+        --worker-boot-disk-size 250 \
+    --num-preemptible-workers 62 \
+        --preemptible-worker-boot-disk-size 250 \
     --scopes 'https://www.googleapis.com/auth/cloud-platform' \
     --project ncbi-sandbox-blast \
     --labels owner=$USER \
