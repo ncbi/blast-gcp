b76346523d71a9e231dfd5f357753f0b73561640
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2018-04-26 10:05:04 -0400

    Set proper pubsub topic

--- pipeline/publish.sh
+++ pipeline/publish.sh
@@ -1,6 +1,6 @@
 PROJECT="ncbi-sandbox-blast"
 #TOPIC="spark-test-topic"
-TOPIC="spark-test-topic"
+TOPIC="run-queue-integration"
 MESSAGE=`cat $1`
 
 gcloud pubsub topics publish $TOPIC --project $PROJECT --message "$MESSAGE"
