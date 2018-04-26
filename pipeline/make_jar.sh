a79c54361f9d53c7e2a6c498ace7bcd8eb369941
Mike Vartanian <Mike.Vartanian@nih.gov>
2018-04-26 08:08:21 -0400

    Build pubsub library

--- pipeline/make_jar.sh
+++ pipeline/make_jar.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-clear
+
+
+if [ ! -d "bigdata-interop" ]; then
+    echo "compiling pubsub library"
+    git clone https://github.com/GoogleCloudPlatform/bigdata-interop.git
+    pushd bigdata-interop/pubsub
+    export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+    export PATH="$JAVA_HOME/bin:$PATH"
+    mvn -q package
+    popd
+fi
+
 echo "compiling java-classes"
 mvn -q package
 exit
