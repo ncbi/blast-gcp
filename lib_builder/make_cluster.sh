a4a4c5dbd522fdec4a06b848b355872f86c618dc
boratyng <boratyng@ncbi.nlm.nih.gov>
2018-05-02 10:13:15 -0400

    Changed pipleline bucket to blast-test-greg

--- lib_builder/make_cluster.sh
+++ lib_builder/make_cluster.sh
@@ -3,7 +3,7 @@ set -o nounset
 set -o pipefail
 set -o errexit
 
-PIPELINEBUCKET=${1:-"gs://blastgcp-pipeline-test"}
+PIPELINEBUCKET=${1:-"gs://blast-test-greg"}
 
 # Note: 100GB enough for NT
 # Larger nodes have faster network, and faster startup
