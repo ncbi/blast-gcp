1ca857a99468937ffacb4b27316ceba46b425367
Mike Vartanian <Mike.Vartanian@nih.gov>
2018-05-11 10:38:50 -0400

    Code reorg

--- experiment_mike/src/main/java/BLAST_DRIVER.java
+++ experiment_mike/src/main/java/BLAST_DRIVER.java
@@ -32,10 +32,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.TreeMap;
 import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.sql.Dataset;
@@ -51,24 +52,23 @@ import org.apache.spark.sql.types.StructType;
 import org.apache.spark.storage.StorageLevel;
 
 public final class BLAST_DRIVER {
-  public static void main(String[] args) throws Exception {
-    BLAST_SETTINGS settings;
-    SparkSession ss;
-    SparkContext sc;
-    JavaSparkContext jsc;
+  private static BLAST_SETTINGS settings;
+  private static SparkSession sparksession;
+  private static JavaSparkContext javasparkcontext; // Only one spark context allowed per JVM
 
+  public static boolean init(final String[] args) {
     if (args.length != 1) {
       System.out.println("settings json-file missing");
-      return;
+      return false;
     }
-    String ini_path = args[0];
+    final String ini_path = args[0];
 
-    String appName = "experiment_mike";
+    final String appName = "experiment_mike";
     settings = BLAST_SETTINGS_READER.read_from_json(ini_path, appName);
     System.out.println(String.format("settings read from '%s'", ini_path));
     if (!settings.valid()) {
       System.out.println(settings.missing());
-      return;
+      return false;
     }
     System.out.println(settings.toString());
 
@@ -90,33 +90,36 @@ public final class BLAST_DRIVER {
     conf.set("spark.locality.wait", settings.locality_wait);
     String warehouseLocation = new File("spark-warehouse").getAbsolutePath();
     conf.set("spark.sql.warehouse.dir", warehouseLocation);
-    conf.set("spark.sql.shuffle.partitions", "886");
+    conf.set("spark.sql.shuffle.partitions", "886"); // TODO: Configurable
     conf.set("spark.default.parallelism", "886");
     conf.set("spark.shuffle.reduceLocality.enabled", "false");
     conf.set("spark.sql.streaming.schemaInference", "true");
 
     builder.config(conf);
-    System.out.println(conf.toDebugString());
+    System.out.println("Configuration is");
+    System.out.println("----------------");
+    System.out.println(conf.toDebugString().replace("\n", "\n  "));
     System.out.println();
     builder.enableHiveSupport();
 
-    ss = builder.getOrCreate();
-    sc = ss.sparkContext();
-    jsc = new JavaSparkContext(sc);
-
-    sc.setLogLevel(settings.spark_log_level);
+    sparksession = builder.getOrCreate();
+    javasparkcontext = new JavaSparkContext(sparksession.sparkContext());
+    javasparkcontext.setLogLevel(settings.spark_log_level);
 
     // send the given files to all nodes
     List<String> files_to_transfer = new ArrayList<>();
     files_to_transfer.add("libblastjni.so");
-    for (String a_file : files_to_transfer) sc.addFile(a_file);
+    for (String a_file : files_to_transfer) {
+      javasparkcontext.addFile(a_file);
+    }
 
-    System.out.println("starting");
+    return true;
+  }
 
-    //        StructType query_schema = StructType.fromDDL("RID string, db string, query_seq
-    // string");
+  private static DataStreamWriter<Row> make_prelim_stream() {
+    System.out.println("making prelim_stream");
 
-    StructType parts_schema = StructType.fromDDL("db string, num int");
+    StructType parts_schema = StructType.fromDDL("db string, partition_num int");
 
     ArrayList<Row> data = new ArrayList<Row>();
 
@@ -124,142 +127,201 @@ public final class BLAST_DRIVER {
       Row r = RowFactory.create("nt", new Integer(i));
       data.add(r);
     }
-    Dataset<Row> parts2 = ss.createDataFrame(data, parts_schema);
-    parts2.createOrReplaceTempView("parts2");
-    Dataset<Row> parts3 =
-        parts2.repartition(886, parts2.col("num")).persist(StorageLevel.MEMORY_AND_DISK_2());
 
-    parts3.show();
-    parts3.createOrReplaceTempView("parts3");
+    Dataset<Row> parts = sparksession.createDataFrame(data, parts_schema);
+    // parts.createOrReplaceTempView("parts");
+    // Paranoia. Want to make sure the underlying RDD is
+    // never recomputed, even if nodes are lost.
+    Dataset<Row> blast_partitions =
+        parts
+            .repartition(886, parts.col("partition_num"))
+            .persist(StorageLevel.MEMORY_AND_DISK_2());
 
-    DataStreamReader query_stream = ss.readStream();
+    blast_partitions.show();
+    blast_partitions.createOrReplaceTempView("blast_partitions");
+
+    DataStreamReader query_stream = sparksession.readStream();
     query_stream.format("json");
-    query_stream.option("maxFilesPerTrigger", 3);
+    query_stream.option("maxFilesPerTrigger", 3); // TODO: Configureable
     query_stream.option("multiLine", true);
     query_stream.option("includeTimestamp", true);
 
-    Dataset<Row> queries = query_stream.json("/user/vartanianmh/requests");
+    Dataset<Row> queries = query_stream.json(settings.hdfs_source_dir);
     queries.printSchema();
     queries.createOrReplaceTempView("queries");
 
     Dataset<Row> joined =
-        ss.sql(
-                "select RID, queries.db, query_seq, num from queries, parts2 where queries.db=parts2.db distribute by num")
+        sparksession
+            .sql(
+                "select RID, queries.db, partition_num, "
+                    + "query_seq, timestamp_hdfs"
+                    + "from queries, blast_partitions "
+                    + "where queries.db=parts2.db "
+                    + "distribute by partition_num")
             .repartition(886);
     joined.createOrReplaceTempView("joined");
     joined.printSchema();
 
-    //        StructType out_schema=StructType.fromDDL("foo string, foo2 string");
+    Integer top_n = settings.top_n;
+    String jni_log_level = settings.jni_log_level;
 
-    Dataset<Row> out2 =
+    Dataset<Row> prelim_search_results =
         joined
             .flatMap(
                 (FlatMapFunction<Row, String>)
                     inrow -> {
-                      long starttime = System.currentTimeMillis();
                       BLAST_LIB blaster = new BLAST_LIB();
                       blaster.log("INFO", "SPARK:" + inrow.mkString(":"));
 
-                      String rid = inrow.getString(0);
-                      String db = inrow.getString(1);
-                      String query_seq = inrow.getString(2);
-                      int num = -999;
-                      if (!inrow.isNullAt(3)) num = inrow.getInt(3);
+                      String rid = inrow.getString(inrow.fieldIndex("RID"));
+                      String db = inrow.getString(inrow.fieldIndex("db"));
+                      int partition_num = inrow.getInt(inrow.fieldIndex("partition_num"));
+                      String query_seq = inrow.getString(inrow.fieldIndex("query_seq"));
 
                       BLAST_REQUEST requestobj = new BLAST_REQUEST();
-                      requestobj.id = "test ";
+                      requestobj.id = rid;
                       requestobj.query_seq = query_seq;
                       requestobj.query_url = "";
-                      requestobj.params = "nt:" + inrow.mkString();
-                      requestobj.db = "nt";
+                      requestobj.db = db;
+                      requestobj.params = "blastn";
                       requestobj.program = "blastn";
-                      requestobj.top_n = 100;
+                      requestobj.top_n = top_n;
                       BLAST_PARTITION partitionobj =
-                          new BLAST_PARTITION("/tmp/blast/db/prefetched", "nt_50M", num, false);
+                          new BLAST_PARTITION(
+                              "/tmp/blast/db/prefetched", "nt_50M", partition_num, false);
 
-                      List<String> result = new ArrayList<>();
+                      List<String> hsp_csvs = new ArrayList<>();
                       if (blaster != null) {
                         BLAST_HSP_LIST[] search_res =
-                            blaster.jni_prelim_search(partitionobj, requestobj, "INFO");
+                            blaster.jni_prelim_search(partitionobj, requestobj, jni_log_level);
 
                         for (BLAST_HSP_LIST S : search_res) {
                           byte[] encoded = Base64.getEncoder().encode(S.hsp_blob);
                           String b64blob = new String(encoded);
-                          String rec = String.format("%d,%s", S.max_score, b64blob);
-                          result.add(rec);
-                        }
-                        long finishtime = System.currentTimeMillis();
+                          // oid, max_score, blob
+                          String rec =
+                              String.format(
+                                  "%s," + // RID
+                                      "%s," + // DB
+                                      "%d," + // partition_num
+                                      "%s," + // query_seq
+                                      "%d," + // oid
+                                      "%d," + // max_score
+                                      "%s" + // blob
+                                      "",
+                                  rid, db, partition_num, query_seq, S.oid, S.max_score, b64blob);
 
-                      } else result.add("null blaster ");
-                      return result.iterator();
+                          hsp_csvs.add(rec);
+                        }
+
+                      } else hsp_csvs.add("null blaster ");
+                      return hsp_csvs.iterator();
                     },
                 Encoders.STRING())
-            .toDF("fromflatmap");
-    out2.createOrReplaceTempView("out2");
-    Dataset<Row> out3 = out2.repartition(1);
+            .toDF("hsp_csv");
+    //        prelim_search_results.createOrReplaceTempView("prelim_search_results");
+    Dataset<Row> prelim_search_1 = prelim_search_results.repartition(1);
 
-    DataStreamWriter<Row> dsw = out3.writeStream();
-    DataStreamWriter<Row> dsw2 =
-        dsw.foreach(
-            new ForeachWriter<Row>() {
-              private TreeMap<Integer, String> tmap;
-              private long starttime;
-              private long finishtime;
-              private PrintWriter out;
-              private long partitionId;
-              private int recordcount = 0;
+    DataStreamWriter<Row> prelim_dsw = prelim_search_1.writeStream();
+    DataStreamWriter<Row> topn_dsw =
+        prelim_dsw
+            .foreach(
+                new ForeachWriter<Row>() {
+                  private long partitionId;
+                  PrintWriter log;
+                  int recordcount = 0;
+                  // HashMap maps RID -> TreeMap (reversed) of scores and HSPlist
+                  // So we can efficiently compute topN scores by RID
+                  HashMap<String, TreeMap<Integer, ArrayList<String>>> score_map;
 
-              @Override
-              public boolean open(long partitionId, long version) {
-                starttime = System.currentTimeMillis();
-                tmap = new TreeMap<Integer, String>();
+                  @Override
+                  public boolean open(long partitionId, long version) {
+                    score_map = new HashMap<String, TreeMap<Integer, ArrayList<String>>>();
 
-                this.partitionId = partitionId;
+                    this.partitionId = partitionId;
 
-                try {
-                  out = new PrintWriter(new FileWriter("/tmp/mike2.log", true), true);
-                  out.println(String.format("open %d %d", partitionId, version));
-                  if (partitionId != 0)
-                    out.println(String.format(" *** not partition 0 %d ??? ", partitionId));
-                } catch (IOException e) {
-                  System.err.println(e);
-                }
-                return true;
-              }
+                    try {
+                      log = new PrintWriter(new FileWriter("/tmp/foreach.log", true), true);
+                      log.println(String.format("open %d %d", partitionId, version));
+                      if (partitionId != 0)
+                        log.println(String.format(" *** not partition 0 %d ??? ", partitionId));
+                    } catch (IOException e) {
+                      System.err.println(e);
+                      return false;
+                    }
+                    return true;
+                  }
 
-              @Override
-              public void process(Row value) {
-                ++recordcount;
-                out.println(String.format(" in process %d", partitionId));
-                out.println("  " + value.mkString(":"));
-                String line = value.getString(0);
-                out.println("  line is " + line);
-                String[] csv = line.split(",");
-                out.println("  has " + csv.length);
-                Integer score = Integer.parseInt(csv[0]);
-                out.println(String.format("score is %d", score));
-                tmap.put(score, csv[1]);
-              }
+                  @Override
+                  public void process(Row value) {
+                    ++recordcount;
+                    log.println(String.format(" in process %d", partitionId));
+                    log.println("  " + value.mkString(":"));
+                    String line = value.getString(0);
+                    log.println("  line is " + line);
+                    String[] csv = line.split(",");
+                    log.println("  has " + csv.length);
+                    String rid = csv[0];
+                    Integer max_score = Integer.parseInt(csv[5]);
+                    log.println(String.format("max_score is %d", max_score));
 
-              @Override
-              public void close(Throwable errorOrNull) {
-                finishtime = System.currentTimeMillis();
-                out.println(String.format(" foreach finished at %d", finishtime));
-                out.println(String.format(" foreach loop took %d ms", finishtime - starttime));
-                out.println(String.format(" saw %d records", recordcount));
-                out.println(String.format(" treemap had %d", tmap.size()));
-                out.println(String.format("close %d", partitionId));
-                out.flush();
-              }
-            });
-    DataStreamWriter<Row> dsw3 = dsw2.outputMode("Append");
+                    if (!score_map.containsKey(rid)) {
+                      TreeMap<Integer, ArrayList<String>> tm =
+                          new TreeMap<Integer, ArrayList<String>>(Collections.reverseOrder());
+                      score_map.put(rid, tm);
+                    }
 
-    StreamingQuery results2 = out3.writeStream().outputMode("append").format("console").start();
+                    TreeMap<Integer, ArrayList<String>> tm = score_map.get(rid);
 
+                    if (!tm.containsKey(max_score)) {
+                      ArrayList<String> al = new ArrayList<String>();
+                      tm.put(max_score, al);
+                    }
+                    ArrayList<String> al = tm.get(max_score);
+
+                    al.add(line);
+                    tm.put(max_score, al);
+                    score_map.put(rid, tm);
+                  }
+
+                  @Override
+                  public void close(Throwable errorOrNull) {
+                    log.println(String.format(" saw %d records", recordcount));
+                    log.println(String.format(" treemap has %d", score_map.size()));
+
+                    for (String rid : score_map.keySet()) {
+                      TreeMap<Integer, ArrayList<String>> tm = score_map.get(rid);
+                      int i = 0;
+                      for (Integer k : tm.keySet()) {
+                        if (i < top_n) {
+                          ArrayList<String> al = tm.get(k);
+                          for (String line : al) {
+                            log.println(String.format("i=%d, line=%s", i, line));
+                          }
+                        } else break;
+
+                        ++i;
+                      }
+                    }
+
+                    //                Integer max_score=score_map.lastKey();
+                    // Find the
+                    log.println(String.format("close %d", partitionId));
+                    log.flush();
+                    return;
+                  }
+                })
+            .outputMode("Append");
+
+    return topn_dsw;
+  }
+
+  private static boolean run_streams(DataStreamWriter<Row> prelim_dsw) {
     System.out.println("starting stream...");
+    StreamingQuery prelim_results = prelim_dsw.outputMode("append").format("console").start();
     try {
       for (int i = 0; i < 9; ++i) {
-        StreamingQuery results = dsw3.start();
+        StreamingQuery results = prelim_dsw.start();
         System.out.println("stream running...");
         Thread.sleep(10000);
         System.out.println(results.lastProgress());
@@ -267,7 +329,31 @@ public final class BLAST_DRIVER {
       }
     } catch (Exception e) {
       System.out.println("Spark exception: " + e);
+      return false;
     }
     System.out.println("That is enough for now");
-  } // run
+
+    return true;
+  }
+
+  private static void shutdown() {
+    javasparkcontext.stop();
+    sparksession.stop();
+  }
+
+  public static void main(String[] args) throws Exception {
+    boolean result;
+    result = init(args);
+    if (!result) {
+      shutdown();
+      return;
+    }
+
+    DataStreamWriter<Row> prelim_stream = make_prelim_stream();
+    result = run_streams(prelim_stream);
+    if (!result) {
+      shutdown();
+      return;
+    }
+  }
 }
