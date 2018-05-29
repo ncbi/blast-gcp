1ad77b54ae26aabd0b1a6df652ffc6cf0e44bd07
Peter Meric <meric@ncbi.nlm.nih.gov>
2018-05-29 10:50:23 -0400

    Uncomment dataproc job submission

--- pipeline/run_spark_docker.sh
+++ pipeline/run_spark_docker.sh
@@ -24,84 +24,84 @@ checkvar=${joborch_output_topic?"${usage}"}
 
 
 INI=$(cat <<-END
+{
+    "appName" : "blast_pipeline",
+    "source" :
     {
-        "appName" : "blast_pipeline",
-        "source" :
+        "pubsub" :
         {
-            "pubsub" :
-            {
-                "project_id" : "PROJECT",
-                "subscript_id" : "PUBSUB",
-                "use" : "true"
-            },
-            "socket" :
-            {
-                "trigger_port" : 10012,
-                "use" : "false"
-            },
-            "hdfs" :
-            {
-                "dir" : "hdfs:///user/%s/requests",
-                "use" : "false"
-            }
+            "project_id" : "PROJECT",
+            "subscript_id" : "PUBSUB",
+            "use" : "true"
         },
-
-        "blastjni" :
+        "socket" :
         {
-            "db" :
-            [
-                {
-                    "selector" : "nt",
-                    "bucket"   : "nt_50mb_chunks",
-                    "pattern"  : "nt_50M",
-                    "extensions" : [ "nhr", "nin", "nsq" ],
-                    "num_partitions" : 887,
-                    "num_locations" : 2
-                },
-                {
-                    "selector" : "nr",
-                    "bucket"   : "nr_50mb_chunks",
-                    "pattern"  : "nr_50M",
-                    "extensions" : [ "phr", "pin", "psq" ],
-                    "num_partitions" : 1086,
-                    "num_locations" : 1
-                }
-            ],
-            "jni_log_level": "DEBUG",
-            "top_n" : 100
+            "trigger_port" : 10012,
+            "use" : "false"
         },
-
-        "result" :
+        "hdfs" :
         {
-            "asn1" :
-            {
-                "bucket" : "RESULTBUCKET"
-            },
-            "status" :
-            {
-                "bucket"  : "RESULTBUCKET"
-            }
-        },
-
-        "spark" :
-        {
-            "transfer_files" : [ "libblastjni.so" ],
-            "shuffle_reduceLocality_enabled" : "false",
-            "scheduler_fair" : "false",
-            "with_locality" : "true",
-            "num_executors" : 400,
-            "num_executor_cores" : 1,
-            "executor_memory" : "1G",
-            "locality_wait" : "30s"
-        },
-
-        "log" :
-        {
-            "log_port" : 10011,
-            "pref_loc" : "false",
-            "part_prep" : "true"
+            "dir" : "hdfs:///user/%s/requests",
+            "use" : "false"
         }
+    },
+
+    "blastjni" :
+    {
+        "db" :
+        [
+            {
+                "selector" : "nt",
+                "bucket"   : "nt_50mb_chunks",
+                "pattern"  : "nt_50M",
+                "extensions" : [ "nhr", "nin", "nsq" ],
+                "num_partitions" : 887,
+                "num_locations" : 2
+            },
+            {
+                "selector" : "nr",
+                "bucket"   : "nr_50mb_chunks",
+                "pattern"  : "nr_50M",
+                "extensions" : [ "phr", "pin", "psq" ],
+                "num_partitions" : 1086,
+                "num_locations" : 1
+            }
+        ],
+        "jni_log_level": "DEBUG",
+        "top_n" : 100
+    },
+
+    "result" :
+    {
+        "asn1" :
+        {
+            "bucket" : "RESULTBUCKET"
+        },
+        "status" :
+        {
+            "bucket"  : "RESULTBUCKET"
+        }
+    },
+
+    "spark" :
+    {
+        "transfer_files" : [ "libblastjni.so" ],
+        "shuffle_reduceLocality_enabled" : "false",
+        "scheduler_fair" : "false",
+        "with_locality" : "true",
+        "num_executors" : 400,
+        "num_executor_cores" : 1,
+        "executor_memory" : "1G",
+        "locality_wait" : "30s"
+    },
+
+    "log" :
+    {
+        "log_port" : 10011,
+        "pref_loc" : "false",
+        "part_prep" : "true"
     }
+}
 END
 )
 
@@ -109,9 +109,8 @@ INI=${INI//PROJECT/$project}
 INI=${INI//PUBSUB/$joborch_output_topic}
 INI=${INI//RESULTBUCKET/$result_bucket_name}
 
-echo "$INI" >> /app/ini_docker.json
+printf "$INI\n" > /app/ini_docker.json
 
-echo gcloud dataproc jobs submit spark \
-    --cluster "$CLUSTER" \
-    --jar sparkblast-1-jar-with-dependencies.jar \
-    /app/ini_docker.json
+gcloud dataproc jobs submit spark --cluster "${CLUSTER}" \
+    --jar sparkblast-1-jar-with-dependencies.jar /app/ini_docker.json
+
