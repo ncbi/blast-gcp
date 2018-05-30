61dcd5e8b4532c1379f9781929504b7616f2b787
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2018-05-30 14:04:02 -0400

    Simplify cluster name

--- lib_builder/make_cluster.sh
+++ lib_builder/make_cluster.sh
@@ -140,7 +140,7 @@ CORES_PER_WORKER=$BEST_PRICE
 config
 
 CMD="gcloud beta dataproc --region us-east4 \
-    clusters create blast-dataproc-$USER-$(date +%Y%m%d-%-I) \
+    clusters create blast-dataproc-$USER \
     --master-machine-type $MASTER \
         --master-boot-disk-size $DISK_PER_MASTER \
     --num-workers ${NUM_WORKERS} \
