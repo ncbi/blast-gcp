0b947c4a4feceb1e1059cac63c74c56403246cb9
Peter Meric <meric@ncbi.nlm.nih.gov>
2018-07-08 23:43:41 -0400

    Pass ini filename as arg to dataproc job

--- pipeline/run_spark_docker.sh
+++ pipeline/run_spark_docker.sh
@@ -14,6 +14,7 @@ Required environment variables:
 [ $# -eq 1 ] || { echo ${usage} && exit 1; }
 
 INI_JSON=${1}
+INI_JSON_FILENAME=${1##*/}
 
 checkvar=${project?"${usage}"}
 checkvar=${region?"${usage}"}
@@ -27,5 +28,5 @@ SPARK_BLAST_CLASS="gov.nih.nlm.ncbi.blastjni.BLAST_MAIN"
 gcloud dataproc jobs submit spark --project ${project} --region ${region} --cluster "${blast_dataproc_cluster_name}" \
     --labels owner=${deploy_user},deploy_id=${deploy_id} \
     --jars ${SPARK_BLAST_JAR} --class ${SPARK_BLAST_CLASS} \
-    --files /etc/blast/dbs.json,libblastjni.so,${INI_JSON} -- ${INI_JSON}
+    --files /etc/blast/dbs.json,libblastjni.so,${INI_JSON} -- ${INI_JSON_FILENAME}
 
