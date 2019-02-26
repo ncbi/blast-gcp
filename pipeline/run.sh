ee7af058798a6461e86e2ae4eb23a577866d36e8
Greg Boratyn <boratyng@ncbi.nlm.nih.gov>
2019-02-26 22:10:34 +0000

    Use libblastjni.so from the gs bucket

--- pipeline/run.sh
+++ pipeline/run.sh
@@ -4,6 +4,6 @@ BC_CLASS="gov.nih.nlm.ncbi.blastjni.BC_MAIN"
 BC_JAR="./target/sparkblast-1-jar-with-dependencies.jar"
 BC_INI="ini.json"
 
-[ -f libblastjni.so ] || ln ../lib_builder/libblastjni.so .
+[ -f libblastjni.so ] || gsutil cp gs://blast-lib/libblastjni.so .
 
 spark-submit --master yarn --class $BC_CLASS $BC_JAR $BC_INI
