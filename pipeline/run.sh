f22073e1c4e96058e4932c83ca338f85eebdddc9
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2019-02-26 16:39:42 -0500

    Make .so available to spark-submit

--- pipeline/run.sh
+++ pipeline/run.sh
@@ -4,4 +4,6 @@ BC_CLASS="gov.nih.nlm.ncbi.blastjni.BC_MAIN"
 BC_JAR="./target/sparkblast-1-jar-with-dependencies.jar"
 BC_INI="ini.json"
 
+[ -f libblastjni.so ] || ln ../lib_builder/libblastjni.so .
+
 spark-submit --master yarn --class $BC_CLASS $BC_JAR $BC_INI
