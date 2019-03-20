dd78ff1deae442860c64371f994823b1e95917fa
Mike Vartanian <Mike.Vartanian@nih.gov>
2019-03-20 12:54:39 -0400

    Diff in correct dir

--- pipeline/run_stability_tests.sh
+++ pipeline/run_stability_tests.sh
@@ -82,6 +82,7 @@ wc -l -- *.asn1.txt | sort > ../asn1.txt.wc.result
 #    echo "Differences in .asn1 output"
 #fi
 
+cd ..
 if diff asn1.txt.wc.expected asn1.txt.wc.result; then
     echo "Differences in .asn1.txt output"
 fi
