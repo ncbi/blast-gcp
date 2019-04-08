7ce7e79cb9c52d67843de950f5a8efddf6db45dc
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2019-04-08 17:17:58 -0400

    Try to auto-detect database to set config file properly

--- pipeline/run.sh
+++ pipeline/run.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+if [ $# -eq 0 ] ; then
+    echo "Usage: $0 <input-file> [nt|nr]"
+    echo "\tif the input file name has blastn or blastp in its name, the database will be set appropriately"
+    exit 0
+fi
+
+DB=${2:-""}
+if [[ $1 =~ *"blastn"* ]] || [[ $1 =~ "nt"* ]] ; then
+    DB=nt
+else
+    DB=nr
+fi
+
+
 BC_CLASS="gov.nih.nlm.ncbi.blastjni.BC_MAIN"
 BC_JAR="./target/sparkblast-1-jar-with-dependencies.jar"
-BC_INI="ini.json"
+BC_INI="$DB-ini.json"
 LOG_CONF="--driver-java-options=-Dlog4j.configuration=file:log4j.properties"
 
 [ -f libblastjni.so ] || gsutil cp gs://blast-lib/libblastjni.so .
