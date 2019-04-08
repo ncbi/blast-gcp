1db395d35f6031148f96145816eb3c7fc6a87500
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2019-04-08 16:44:17 -0400

    Minor fix to delete_cluster.sh

--- lib_builder/delete_cluster.sh
+++ lib_builder/delete_cluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 # delete_cluster.sh: terminates a cluster started via make_cluster_fixed_config.sh
 set -euo pipefail
-gcloud dataproc clusters delete blast-dataproc-$USER --region us-east4
+yes | gcloud dataproc clusters delete blast-dataproc-$USER --region us-east4
